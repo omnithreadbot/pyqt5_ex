f5214623f6ac523e4570f872b1db35fb89ac6bde
omnithreadbot <omnithreadbot@gmail.com>
2025-07-01 17:25:57 +0900

    blank_window파일 추가

---
+++ blank_window.py
@@ -0,0 +1,24 @@
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # 창의 이름을 설정
+        self.setWindowTitle('My App')
+        # 창을 띄우는 위치
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
+
+
+if __name__ == '__main__':
+   app = QApplication(sys.argv)
+   ex = MyApp()
+   sys.exit(app.exec_())
